b948c1f9bd7f2e7664f01f58d591b969391d82cb
wonjiwe <75560526+wonjiwe@users.noreply.github.com>
2020-12-06 20:26:21 +0800

    20201206

--- js/practice.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    var offset = 300;       /*往下滾到哪就出現TOP鈕*/
-    var duration = 2000;     /*往上跑的速度，數字越大越慢*/
-    $('#news').click(function () {
-        $('body').animate({
-            scrollTop: 0    /*往上跑到哪的位置*/
-        }, duration);
-    })
-});
